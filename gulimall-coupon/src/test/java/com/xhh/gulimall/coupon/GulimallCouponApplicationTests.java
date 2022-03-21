daf6b65f8d648e14e2d4107ff2ff7dccd0557ad3
xiaohuihui <2929836729@qq.com>
2022-03-21 23:52:05 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/xhh/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhh.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
